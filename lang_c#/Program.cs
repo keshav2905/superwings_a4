a60de1e3034e2ef190096716933d10f454a83828
keshav2905 <keshav.verma@rmgx.in>
2021-12-16 08:33:57 +0530

    task in c#

---
+++ lang_c#/Program.cs
@@ -0,0 +1,30 @@
+﻿int calculateCharges(int distance) {
+
+    int charges = 0;
+
+    while(distance > 15) {
+        charges = charges + 15;
+        distance = distance - 1;
+    }
+
+    while(distance > 5) {
+        charges = charges + 18;
+        distance = distance - 1;
+    }
+
+    while(distance > 2) {
+        charges = charges + 20;
+        distance = distance - 1;
+    }
+
+    charges= charges + 50;
+
+    return charges;
+}
+
+
+int distance;
+Console.WriteLine("Enter distance in KM.: ");
+distance = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Total Charges: " + calculateCharges(distance));
